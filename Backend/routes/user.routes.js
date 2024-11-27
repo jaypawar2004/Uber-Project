c801d16bef76fd4abb5bacabba17210e5f85763b
jaypawar2004 <pawarjay7000@gmail.com>
2024-11-27 13:50:06 +0530

    first commit

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { body } = require('express-validator');
+const userController = require('../controllers/user.controller');
+
+
+router.post('/register', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname').isLength({min: 3}).withMessage
+    ('3 se jada likh bhai'),
+    body('password').isLength({min :6}).withMessage('Password 6 se jada likh')
+],
+userController.registerUser
+)
+
+module.exports = router;
